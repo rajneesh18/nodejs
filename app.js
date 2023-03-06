68b2f93b3988150f01b17606e0140d3c23dd506d
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-03-06 12:05:14 +0530

    Add & finish the controller

--- app.js
+++ app.js
@@ -4,26 +4,24 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
+const errorController = require("./controllers/error");
 
+const rootDir = require('./util/path');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-const shopData = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
-app.use(shopData);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404Page);
 
 const server = http.createServer(app);
 server.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,10 @@
 const express = require('express');
 
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const products = [];
+router.get('/add-product',productsController.getAddProduct);
+router.post('/add-product', productsController.postAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', activeAddProduct: true, productCSS: true })
-});
-
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-})
-
-// module.exports = router;
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
