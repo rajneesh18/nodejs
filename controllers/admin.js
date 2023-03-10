11f7d5fbfc12ec812c2961dce6daaf5cc8899d80
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-03-10 17:40:34 +0530

    Editing the product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 }
@@ -32,7 +32,15 @@ exports.getEditProduct = (req, res) => {
 }
 
 exports.postEditProduct = (req, res) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    updatedProduct.save();
 
+    res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,6 @@ router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product');
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
