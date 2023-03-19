93e781b8861fa5f8f05eb52d7fbbe4a6224795f4
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-03-19 20:53:10 +0530

    Retrieving Data & Finding Products - Sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,11 +2,12 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
-            res.render('shop/product-list', { prods: rows, pageTitle: 'All Products', path:'/products' });
+    Product
+        .findAll()
+        .then(products => {
+            res.render('shop/product-list', { prods: products, pageTitle: 'All Products', path:'/products' });
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
 }
 
 exports.getProduct = (req, res) => {
@@ -19,9 +20,10 @@ exports.getProduct = (req, res) => {
 }
 
 exports.getIndex = (req, res) => {
-    Product.fetchAll()
-        .then(([rows, fieldData]) => {
-            res.render('shop/index', { prods: rows, pageTitle: 'Shop', path:'/' });
+    Product
+        .findAll()
+        .then(products => {
+            res.render('shop/index', { prods: products, pageTitle: 'Shop', path:'/' });
         })
         .catch(err => console.log(err));
 }
