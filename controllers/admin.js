b025b1bb7b67e8081ffccb1f1afe54915488294d
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-03-21 12:51:07 +0530

    Using Magic Association Method - Sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,14 @@ exports.postAddProduct = (req, res) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    Product.create({
+    req.user.createProduct({
         title: title,
         imageUrl: imageUrl,
         price: price,
-        description: description
-    }).then(result => {
+        description: description,
+        userId: req.user.id
+    })
+    .then(result => {
         console.log('Created Product');
         res.redirect('/admin/add-product')
     }).catch(err => console.log(err));
