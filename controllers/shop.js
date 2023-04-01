711c54ccee5c6c3810c9c26b916b3d0919825c80
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-04-01 23:09:10 +0530

    Fetching All Products - MongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.getProducts = (req, res) => {
     Product
-        .findAll()
+        .fetchAll()
         .then(products => {
             res.render('shop/product-list', { prods: products, pageTitle: 'All Products', path:'/products' });
         })
@@ -20,7 +20,7 @@ exports.getProduct = (req, res) => {
 
 exports.getIndex = (req, res) => {
     Product
-        .findAll()
+        .fetchAll()
         .then(products => {
             res.render('shop/index', { prods: products, pageTitle: 'Shop', path:'/' });
         })
