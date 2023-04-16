40b1f1ec30be234b53df89b60ad3f74fb643b702
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-04-16 20:48:01 +0530

    Updating Products - Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -43,9 +43,15 @@ exports.postEditProduct = (req, res) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
 
-    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
-    product
-        .save()
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+
+            return product.save();
+        })
         .then(result => {
             console.log('Updated Product');
             res.redirect('/admin/products');
@@ -64,7 +70,7 @@ exports.postDeleteProduct = (req, res) => {
 }
 
 exports.getProducts = (req, res) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
