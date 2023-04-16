6408cf181c56fddff76d875489bce4e0baa65edb
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-04-16 21:21:46 +0530

    Adding and Using a User Model - Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require("./controllers/error");
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const rootDir = require('./util/path');
 const app = express();
@@ -19,13 +19,13 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('64292e06d78878c4f23a6fce')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         }).catch(err => console.log(err));
-// })
+app.use((req, res, next) => {
+    User.findById('643c186628f7d68b362e7910')
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,18 @@ app.use(errorController.get404Page);
 mongoose
     .connect('mongodb+srv://rkshukla:N9wMbEkj46DqwicN@cluster0.9zjumcl.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
+        User.findOne().then(user => {
+            if(!user) {
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+            }
+            user.save();
+        })
         app.listen(3000);
     })
     .catch(err => {
