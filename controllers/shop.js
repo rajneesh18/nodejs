8394e554595efac23bf83515f09556a42a1d8e06
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2023-04-20 22:42:58 +0530

    Loading the Cart - Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -30,8 +30,9 @@ exports.getIndex = (req, res) => {
 
 exports.getCart = (req, res) => {
     req.user
-        .getCart()
-        .then(products => {
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items;
             res.render('shop/cart', {
                 path: '/cart',
                 pageTitle: 'Your Cart',
